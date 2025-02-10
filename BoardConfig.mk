f64bafb43f05f9c04dd31abee8530971d16c135c
resparkstar <39150614+resparkstar@users.noreply.github.com>
2025-02-10 23:33:24 +0800

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,16 +1,7 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 DEVICE_PATH := device/meizu/m1note
 
-# For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
-# Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
@@ -25,17 +16,13 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
 
-# APEX
 OVERRIDE_TARGET_FLATTEN_APEX := true
 
-# Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := m1note
 TARGET_NO_BOOTLOADER := true
 
-# Display
 TARGET_SCREEN_DENSITY := 480
 
-# Kernel
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_PAGESIZE := 2048
@@ -47,13 +34,11 @@ BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_KERNEL_CONFIG := m1note_defconfig
 TARGET_KERNEL_SOURCE := kernel/meizu/m1note
 
-# Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
 ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 endif
 
-# Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_BOOTIMAGE_PARTITION_SIZE := 12326912
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 12326912
@@ -63,22 +48,17 @@ BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
 
-# Platform
 TARGET_BOARD_PLATFORM := mt6752
 
-# Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
-# Security patch level
 VENDOR_SECURITY_PATCH := 2021-08-01
 
-# Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
-# TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
