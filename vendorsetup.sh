953924b7eec84161986972c354a845adb04ba088
resparkstar <39150614+resparkstar@users.noreply.github.com>
2025-02-10 23:16:18 +0800

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,2 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 add_lunch_combo omni_m1note-userdebug
 add_lunch_combo omni_m1note-eng
